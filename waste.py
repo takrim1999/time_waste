d02878935750dda3657555bcd3635b67aad1f6a2
takrim1999 <takrimulalom@gmail.com>
2022-01-19 02:58:11 +0600

    Done for now without colors

--- waste.py
+++ waste.py
@@ -1,62 +1,37 @@
 #!/usr/bin/python3
+from os import lstat
+import random
 from turtle import *
-# This is the decimal value of FFFFFF, white. 16777216
-
-# Most important thing here is color,
-# Thats why I'm writing it.
-# I need to make a function with instruction that will show how to brute force colorcodes colorfully.
-
-def init():
-    shape("arrow")
-    pensize(50)
-    shapesize(15)
-    setpos(0,-200)
-    speed(10)
-
-def draw(col):
-    begin_fill()
+from unicodedata import name
+speed(0)
+colormode(255)
+pensize(20)
+color("white")
+setpos(0,-200)
+# shape("arrow")
+# shapesize(5)
+global lst
+lst = [(255,255,255)]
+def write_name(col):
+    speed(0)
+    color(lst[0])
+    setpos(0,-40)
     color(col)
+    write("DIE\nMF!",align='center',font=('Arial',26,'normal'))
+    color(lst[0])
+    setpos(0,-200)
+    lst.pop(0)
+
+# textinput("NIM","NAME OF FIRST PLAYER")
+for i in range(100):
+    
+    col = tuple([random.randint(0,255),random.randint(0,255),random.randint(0,255)])
+    lst.append(col)
+    write_name(col)
+    speed(10)
+    begin_fill()
+    color(col) 
     circle(200)
     end_fill()
 
-def good_color(time):
-    ## First we can brute force RGB indiividually.
-    unit_time = 4
-    desired_colors = int(int(time)/unit_time)
-    highest_color = 2**8
-    variations = 9
-    if desired_colors < variations:
-        variations = desired_colors
-
-    interval = int(highest_color/desired_colors)
-    for j in range(variations):
-        if j==0:
-            for i in range(0,highest_color,interval):
-                draw("#0000"+"0"*(2-len(hex(i)[2:]))+hex(i)[2:])
-        elif j==1:
-            for i in range(0,highest_color,interval):
-                draw("#00"+"0"*(2-len(hex(i)[2:]))+hex(i)[2:]+"00")
-        elif j==2:
-            for i in range(0,highest_color,interval):
-                draw("#"+"0"*(2-len(hex(i)[2:]))+hex(i)[2:]+"0000")
-        elif j==3:
-            for i in range(0,highest_color,interval):
-                draw("#00FF"+"0"*(2-len(hex(i)[2:]))+hex(i)[2:])
-        elif j==4:
-            for i in range(0,highest_color,interval):
-                draw("#FF00"+"0"*(2-len(hex(i)[2:]))+hex(i)[2:])
-        elif j==5:
-            for i in range(0,highest_color,interval):
-                draw("#FF"+"0"*(2-len(hex(i)[2:]))+hex(i)[2:]+"00")
-        elif j==6:
-            for i in range(0,highest_color,interval):
-                draw("#00"+"0"*(2-len(hex(i)[2:]))+hex(i)[2:]+"FF")
-        elif j==7:
-            for i in range(0,highest_color,interval):
-                draw("#"+"0"*(2-len(hex(i)[2:]))+hex(i)[2:]+"FF00")
-        elif j==8:
-            for i in range(0,highest_color,interval):
-                draw("#"+"0"*(2-len(hex(i)[2:]))+hex(i)[2:]+"00FF")
-init()
-good_color(input("how many seconds you want to waste?\n>"))
 done()
